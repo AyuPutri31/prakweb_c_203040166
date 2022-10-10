bc337ce3ff1cc89d7ab486615a0c02fbd3b83e51
AyuPutri31 <ayupuutri0@gmail.com>
2022-10-10 08:15:06 +0700

    menyelesaikan video 6 mvc asset dan membenarkan app.php

---
+++ mvc/app/core/Constants.php
@@ -0,0 +1,6 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_c_203040166/mvc/public');
+
+define('DEFAULT_CONTROLLER', 'Home');
+define('DEFAULT_METHOD', 'index');
--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,2 +1,9 @@
-  <h1>About Me</h1>
-  <p>Halo, nama saya <?= $data['nama']; ?> , umur saya <?= $data['umur']; ?> tahun saya seorang <?= $data['pekerjaan']; ?></p>
+<body class="container bg-dark">
+
+  <div class="bg-light p-5 rounded mt-3">
+    <h1>About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpeg" alt="Ayu Putri" width="200" class="rounded-circle shadow">
+    <p class="lead">Halo, nama saya <?= $data['nama']; ?> , umur saya <?= $data['umur']; ?> tahun saya seorang <?= $data['pekerjaan']; ?></p>
+    <a class="btn btn-lg btn-primary" href="/docs/5.2/components/navbar/" role="button">View navbar docs &raquo;</a>
+  </div>
+</body>
